2d896c05523d5e8395ab5618900b36c48baf4a5e
Josh T. Bernstein <13294526+joshtbernstein@users.noreply.github.com>
2022-07-07 19:04:02 +0000

    Initial Commit🎉

---
+++ scripts/sysprep.cmd
@@ -0,0 +1,5 @@
+IF EXIST a:\unattend.xml (
+  %WINDIR%\system32\sysprep\sysprep.exe /generalize /oobe /shutdown /quiet /unattend:a:\unattend.xml
+) else (
+  %WINDIR%\system32\sysprep\sysprep.exe /generalize /oobe /shutdown /quiet  
+)
