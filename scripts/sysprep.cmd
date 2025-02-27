76981037be48792fa71188f60b16b922604949a0
Josh Bernstein <13294526+joshtbernstein@users.noreply.github.com>
2025-02-27 08:53:21 -0800

    Add disable_winrm.ps1

--- scripts/sysprep.cmd
+++ scripts/sysprep.cmd
@@ -1,3 +1,5 @@
+Powershell.exe -ExecutionPolicy Bypass -File A:\disable_winrm.ps1
+
 IF EXIST a:\unattend.xml (
   %WINDIR%\system32\sysprep\sysprep.exe /generalize /oobe /shutdown /quiet /unattend:a:\unattend.xml
 ) else (
